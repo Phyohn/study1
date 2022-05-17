6b4dc0a00a60030d2262e32acc234e2277ed09cf
MTKSH <friktion0001@gmail.com>
2022-05-17 20:06:12 +0900

    kousinn

--- study1.py
+++ study1.py
@@ -1,6 +1,7 @@
 #study1.py
 from bs4 import BeautifulSoup
 import csv
+from glob import glob
 import os
 import pathlib
 import re
@@ -8,36 +9,32 @@ import requests
 import shutil
 import zipfile
 from pprint import pprint
+import copy
 import collections
 from tqdm import tqdm
+
 
 top_d = "/Users/mac2018/Applications/Collection/study1/"
 #TOP_DIR = os.path.dirname(__file__)
 TOP_DIR = os.path.dirname(top_d)
 ZIP_DIR = os.path.join(TOP_DIR, 'zip/')
-CSV_DIR = os.path.join(TOP_DIR, 'csv/')
-README_SUM_DIR = os.path.join(TOP_DIR, 'readme_sum/')
 
-'''
-#download function
-def download_file(url):
-    filename = url.split('/')[-1]
-    r = requests.get(url, stream=True)
-    with open(filename, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk:
-                f.write(chunk)
-                f.flush()
-        return filename
-    return False
-'''
+def yes_no_input():
+	while True:
+		choice = input("        OK? yes' or 'no' [y/N]:  ( q = quit )").lower()
+		if choice in ['y', 'ye', 'yes']:
+			return True
+		elif choice in ['n', 'no']:
+			return False
+		elif choice in ['q', 'Q']:
+			return quit()
 
-#download function to zipfolder
+#download function to zipfolder with tqdm
 def download_file(url):
     filename = os.path.join(ZIP_DIR, url.split('/')[-1])
     r = requests.get(url, stream=True)
     with open(filename, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024):
+        for chunk in tqdm(r.iter_content(chunk_size=1024)):
             if chunk:
                 f.write(chunk)
                 f.flush()
@@ -46,41 +43,41 @@ def download_file(url):
 
 #os.makedirs
 os.makedirs(ZIP_DIR, exist_ok=True)
-os.makedirs(CSV_DIR, exist_ok=True)
-os.makedirs(CSV_DIR + 'trip', exist_ok=True)
-os.makedirs(CSV_DIR + 'station', exist_ok=True)
-os.makedirs(CSV_DIR + 'any', exist_ok=True)
-os.makedirs(README_SUM_DIR, exist_ok=True)
 
 #Parse html
-path = "Index of bucket _divvy-tripdata_.html"
+#sorce_url "https://divvy-tripdata.s3.amazonaws.com/index.html"
+#r = requests.get('https://divvy-tripdata.s3.amazonaws.com/index.html')
+#soup = BeautifulSoup(r.text, 'lxml')  #ajax
+path = 'Index of bucket "divvy-tripdata".html'
 soup = BeautifulSoup(open(path), 'html.parser')
-pprint(soup)
+#pprint(soup)
 
 #example <a href="https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2017_Q1Q2.zip">Divvy_Trips_2017_Q1Q2.zip</a>
 #example  element.get('href') == 'https://divvy-tripdata.s3.amazonaws.com/index.html'
 url_list = [element.get('href') for element in soup.find_all('a')]
 print(f'html_tag_a {len(url_list)}')
+
 #sort url_list_by_filename re.sub
-#{zeroume(url):url,'2020100000':'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip'}
 
 #re.sub('\D', '', url.split('/')[-1]) is NG 2020_10 = 2020Q1
 #OK re.sub('[^0-9Q]', '', url.split('/')[-1])
 
-def zeroume(url):
+def short_name(url):
 	s = re.sub('[^0-9Qq]', '', url.split('/')[-1])
 	if s == '':
 		s =1
 	z = str(s).ljust(10, '0')
 	return z
 
-
-dict = {zeroume(url) : url for url in url_list}
+#{short_name(url):url,'2020100000':'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip'}
+dict = {short_name(url) : url for url in url_list}
 sorted_dict = sorted(dict.items())
 sorted_url_l = [ v for k, v in sorted_dict]
-print(f'sorted {len(sorted_url_l)}')
+print(f'sorted {len(sorted_url_l)}url_link')
+
 # ext_check
 ext_list = [url.split('.')[-1] for url in sorted_url_l]
+
 #<class 'collections.Counter'> 
 # print(c['a']) == 4 , c.keys() == dict_keys(['a', 'b', 'c']), c.values() == dict_values([4, 1, 2]), c.items() == dict_items([('a', 4), ('b', 1), ('c', 2)])
 ext_d = collections.Counter(ext_list)
@@ -89,32 +86,80 @@ for ext in list(ext_d) :
 	print(f'拡張子"{ext}"は{ext_d[ext]}リンク存在します')
 
 #target_ext is 'zip'
-target_ext = [x for x in list(ext_d) if x == 'zip' ]
+target_l = [ url.replace('\n', '') for url in sorted_url_l if url.split('.')[-1] == 'zip' ]
 
-target_l =[]
-for url in sorted_url_l:
-	item_ex = url.split('.')[-1]
-	for x in target_ext:
-		if item_ex == x :
-			target_l.append(url)
+total_len = len(target_l)
 
-print(f'{len(target_l)}箇所の"zip"リンク取得')
+#pastdata check inside link.txt
+paths = glob(os.path.join(ZIP_DIR, '*.*'))
+txt_l = [path for path in paths if path.split('.')[-1] == 'txt']
 
-#download all zip
+if len(txt_l) == 0 :
+	print(f'新規ダウンロード。全{total_len}fileを開始しますか？')
+	if yes_no_input():
+		with open(ZIP_DIR + 'link.txt', 'w', newline='\n') as f:
+			for url in target_l:
+				f.write("%s\n" % url)
+				download_file(url)
+		print(f'全{total_len}fileを新規ダウンロードしました')
+		print(f'全{total_len}LINKを記入、link.txtを新規作成しました')
+		quit()
+	else :
+		with open(ZIP_DIR + 'link.txt', 'w', newline='\n') as difffile:
+			difffile.write('\n'.join(target_l))
+		print(f'全{total_len}LINKを記入、link.txtを新規作成しました')
+		quit()
+else:
+	pass
 
 
+#check_l = target_l + link.txt
+check_l = copy.copy(target_l)
+if len(txt_l) != 0 :
+	for txt in txt_l:
+		with open(txt, 'r') as f:
+			lines = f.readlines()
+			lines = [ line.replace('\n', '') for line in lines ]
+			check_l.extend(lines)
 
-#make link text ver
-with open('link.txt', 'w') as f:
-	for url in url_list:
-		f.write("%s\n" % url)
-		#download_file(url)
+#filename_d{,,,.zip:2, ,,,.zip: 2 , ,,,.zip : 1 }  value == 1 is newitem
+filename = {}
+filename_d = collections.Counter(check_l)
 
-"""
-for url in target_l:
-	download_file(url)
-"""
+new = [ k for k, v in filename_d.items() if v == 1 and k.split('.')[-1] == 'zip']
 
-print(f'{len(target_l)}files downloadone!')
+if len(new) > 0:
+	print('差分のみダウンロードしますか？')
+	if yes_no_input():
+		for n in new:
+			download_file(n)
+			name = n.split('/')[-1]
+			print(f'{name}を保存しました')
+		with open(ZIP_DIR + 'link.txt', 'a', newline='\n') as difffile:
+			difffile.write("%s\n" % '\ndiff')
+			difffile.write('\n'.join(new))
+		print(f'差分{len(new)}LINKをlink.txtに追記しました')
+	else:
+		with open(ZIP_DIR + 'link.txt', 'a', newline='\n') as difffile:
+			difffile.write("%s\n" % '\ndiff')
+			difffile.write('\n'.join(new))
+		print(f'差分{len(new)}LINKをlink.txtに追記しました')
+else:
+	pass
+
+if len(new) == 0:
+	print('更新はありません')
+	print('全てダウンロードしますか？')
+	if yes_no_input():
+		for url in target_l:
+			download_file(url)
+		print(f'全{total_len}fileを再ダウンロードしました')
+	else :
+		pass
+else:
+	pass
+
+print(f'TOTAL{total_len}箇所の"zip"リンク取得')
+print(f'Total Zip files {total_len}files downloadone!')
 
 quit()
